733c0ef05ffa65dcb5e9a88fc9673f47bb9d7e52
Panos Sakkos <panos.sakkos@gmail.com>
2014-03-04 19:04:21 +0200

    Added interactive console and execution of queries from files.

--- JavaSQLParser/src/di/madgik/cloudb/adapter/demo/Application.java
+++ JavaSQLParser/src/di/madgik/cloudb/adapter/demo/Application.java
@@ -5,22 +5,53 @@ import com.foundationdb.sql.query.SQLQueryParser;
 import di.madgik.cloudb.adapter.CloudQuery;
 import di.madgik.cloudb.adapter.Cloudify;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 /**
  * Created by panossakkos on 2/21/14.
  */
 
 public class Application {
 
-    private static String query = " select count(l.orderkey) " +
-                                  " from lineitem l " +
-                                  " where l.orderkey > 100";
+    private static String QUERIES_FOLDER = "Queries/";
 
     public static void main (String []args) throws Exception {
 
-        SQLQuery query = SQLQueryParser.parse(Application.query);
+        String command;
+        Scanner scanner = new Scanner(System.in);
 
-        CloudQuery cloudQuery = Cloudify.toCloud(query);
+        System.out.print("> ");
+        while ((command = scanner.nextLine()) != null) {
+            String queries[] = command.split(" ");
 
-        System.out.println(cloudQuery);
+            for (String query : queries) {
+
+                if (query.compareTo("q") == 0) {
+                    return;
+                }
+
+                cloudifyQueryFromFile(query);
+            }
+
+            System.out.print("> ");
+        }
+    }
+
+    private static void cloudifyQueryFromFile(String fileName) {
+        try {
+            String content = new Scanner(new File(Application.QUERIES_FOLDER + fileName)).useDelimiter("\\Z").next();
+
+            SQLQuery query = SQLQueryParser.parse(content);
+
+            CloudQuery cloudQuery = Cloudify.toCloud(query);
+
+            System.out.println(cloudQuery);
+
+        }
+        catch(Exception exception) {
+            System.err.println(exception.getMessage());
+        }
     }
 }
