cb5ff4a3056b008f3636daa359bc0d6f239b3d42
Panos Sakkos <panos.sakkos@gmail.com>
2014-05-06 18:37:17 +0300

    added query3

--- CloudAdapter/Queries/cloudb_queries/cloudb_query3.sql
+++ CloudAdapter/Queries/cloudb_queries/cloudb_query3.sql
@@ -1,17 +1,18 @@
-SELECT  internal.o_orderpriority, sum( count_2) as order_count 
-FROM  
-	(
-		SELECT  leaf.o_orderpriority, sum( count_1) as count_2 
-		FROM
-		(
-			SELECT  o.o_orderpriority, count( distinct l.l_orderkey) as count_1 
-			FROM  lineitem l, orders o 
-			WHERE  o.o_orderdate >= '1993-03-01' AND o.o_orderdate < '1993-06-01' AND l.l_orderkey = o.o_orderkey AND l.l_commitdate < l.l_receiptdate 
-			GROUP BY  o.o_orderpriority 
-			ORDER BY  o.o_orderpriority	
-		) as leaf  
-		GROUP BY  leaf.o_orderpriority 
-		ORDER BY  leaf.o_orderpriority
-	) as internal 
-GROUP BY  internal.o_orderpriority
-ORDER BY  internal.o_orderpriority; 
+DISTRIBUTED CREATE TEMPORARY TABLE leaf as
+SELECT  o.o_orderpriority as o_orderpriority, count( l.l_orderkey) as count_1
+FROM  lineitem l, orders o
+WHERE  o.o_orderdate >= '1993-03-01' AND o.o_orderdate < '1993-06-01' AND l.l_orderkey = o.o_orderkey AND l.l_commitdate = l.l_receiptdate
+GROUP BY  o_orderpriority
+ORDER BY  o_orderpriority;
+
+DISTRIBUTED CREATE TEMPORARY TABLE internal as
+SELECT  o_orderpriority as o_orderpriority, sum( count_1) as count_2
+FROM  leaf
+GROUP BY  o_orderpriority
+ORDER BY  o_orderpriority;
+
+DISTRIBUTED CREATE TEMPORARY TABLE root to 1 as
+SELECT  o_orderpriority as o_orderpriority, sum( count_2) as order_count
+FROM  internal
+GROUP BY  o_orderpriority
+ORDER BY  o_orderpriority;
